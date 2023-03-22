3e0c28c9fa8745c7eba99089d59fbf755e0b9831
Enrique Cherot <enrique.cherot2@gmail.com>
2023-03-21 21:47:19 -0300

    Corregimos el root, agregamos footer y view ingredientes

--- script/script.js
+++ script/script.js
@@ -3,13 +3,14 @@ let ingredientes = new Array();
 let contador = 0;
 
 while (true){
-    let ingrediente = prompt("¿Qué ingredientes disponés para cocinar?");
+    let ingrediente = prompt("¿Qué ingredientes disponés para cocinar?").toLowerCase();
     ingredientes.push(ingrediente);
-    let agregar = prompt("¿Quieres agregar otro ingrediente?");
+    let agregar = prompt("¿Quieres agregar otro ingrediente?").toLowerCase();
     contador ++;
     if(agregar == "si"){
         alert("Perfecto, continuamos!")
     } else {
+        alert("¡Genial, terminamos!")
         break;
     }
 
