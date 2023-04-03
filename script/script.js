c45c3e7536e389fe2c932e88c7703910e2d2eccc
Enrique Cherot <enrique.cherot2@gmail.com>
2023-04-02 21:11:13 -0300

    Pre-entrega1 Enrique Cherot

--- script/script.js
+++ script/script.js
@@ -2,24 +2,28 @@
 let ingredientes = new Array();
 let contador = 0;
 
+function pedirNombre() {
+    let nombreIngresado = prompt("Hola, ¿Cómo te llamas?");
+    alert("Así que eres " + nombreIngresado);
+}
+pedirNombre();
+
 while (true){
-    let ingrediente = prompt("¿Qué ingredientes disponés para cocinar?").toLowerCase();
-    ingredientes.push(ingrediente);
-    let agregar = prompt("¿Quieres agregar otro ingrediente?").toLowerCase();
-    contador ++;
-    if(agregar == "si"){
-        alert("Perfecto, continuamos!")
-    } else {
-        alert("¡Genial, terminamos!")
-        break;
-    }
-
+    let ingrediente = prompt("¿Qué ingredientes disponés para cocinar?");
+        if(ingrediente==""|| ingrediente == null|| ingrediente==undefined || ingrediente==" ") {
+            alert("El ingrediente ingresado no es correcto. Por favor ingresa nuevamente el ingrediente")
+        } else{
+            ingredientes.push(ingrediente.toLowerCase());
+            let agregar = prompt("¿Quieres agregar otro ingrediente?").toLowerCase();
+            contador ++;
+            if(agregar == "si"){
+                alert("Perfecto, continuamos!")
+            } else {
+                alert("¡Genial, terminamos!")
+                break;
+            }
+        }
 }
 
-for (let i = 0; i < contador; i++){
-    console.log("ingredientes: " + ingredientes)
-}
-
-//La idea es comparar el array ingredientes con el array de ingredientes de recetas para que traiga la receta que puede hacer
-//siempre que cumpla x % de ingredientes.
+alert("ingredientes ingresados: " + ingredientes);
 
